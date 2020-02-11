6bbeb0172afc664dd48267a60f6008a6f539b368
Angelo Mager <angelomager@MacBook-Pro-van-Peter.local>
2020-02-11 16:29:00 +0100

    Added product and product detail models, controller & tables

---
+++ database/migrations/2020_02_11_145213_create__product_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('modelNumber');
+            $table->text('productInfo');
+            $table->string('imgLink')->nullable();
+            $table->string('pdfLinkNl')->nullable();
+            $table->string('pdfLinkEn')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_product_details');
+    }
+}
